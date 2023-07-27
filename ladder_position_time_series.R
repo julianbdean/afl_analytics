4179b214b492e02404800cc7076cf22cbc959bd8
Julian <julian.dean@gmail.com>
2023-07-27 04:34:35 +0300

    Update ladder_position_time_series.R
    redundant package

--- ladder_position_time_series.R
+++ ladder_position_time_series.R
@@ -1,5 +1,5 @@
 # Check each package and install if not installed
-for (package_name in c("tidyverse", "shiny", "rvest", "polite")) {
+for (package_name in c("tidyverse", "shiny", "rvest")) {
   if (!require(package_name, character.only = TRUE)) {
     # If not installed, install it
     install.packages(package_name, dependencies = TRUE)
